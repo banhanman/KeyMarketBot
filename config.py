16266c9b8e45126443fa118f9ca698903948cde8
hanman <100200140+banhanman@users.noreply.github.com>
2025-06-26 18:14:25 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Конфигурационные параметры
+TOKEN = "ВАШ_TELEGRAM_BOT_TOKEN"
+PAYMENT_TOKEN = "ВАШ_ПЛАТЕЖНЫЙ_ТОКЕН"  # От @BotFather через команду /mybots
+ADMIN_ID = 123456789  # Ваш ID для уведомлений
